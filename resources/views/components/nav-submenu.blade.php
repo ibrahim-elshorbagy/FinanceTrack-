5aed0718b9a5d1dcfc0d8c3dd22dc24a4c084eb2
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-08-29 21:36:59 +0300

    fix submenu

--- resources/views/components/nav-submenu.blade.php
+++ resources/views/components/nav-submenu.blade.php
@@ -3,7 +3,7 @@
 <div x-data="{ isExpanded: {{ $active ? 'true' : 'false' }} }" class="flex flex-col">
     <button type="button"
             x-on:click="isExpanded = ! isExpanded"
-            class="flex items-center justify-between rounded-xl gap-2 px-2 py-1.5 text-sm font-medium underline-offset-2 focus:outline-hidden focus-visible:underline"
+            class="flex items-center justify-between rounded-xl gap-2 pr-2 py-1.5 text-sm font-medium underline-offset-2 focus:outline-hidden focus-visible:underline"
             x-bind:class="isExpanded ? 'text-black bg-purple-500/10 dark:text-neutral-100 dark:bg-purple-400/10' : 'text-neutral-800 hover:bg-purple-500/5 hover:text-black dark:text-neutral-300 dark:hover:bg-purple-400/5 dark:hover:text-neutral-100'">
         <i class="fa-solid {{ $icon }} size-5 shrink-0"></i>
         <span class="mr-auto text-left">{{ $title }}</span>
